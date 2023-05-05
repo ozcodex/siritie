d49a4115c90fa72a0e75008629636710c5743c41
Oscar Bernal <oscarbernal93@gmail.com>
2023-05-05 17:15:09 -0500

    Add salt water support to watering can

--- mods/tech/common/watering_can.lua
+++ mods/tech/common/watering_can.lua
@@ -65,32 +65,43 @@ liquid_store.register_stored_liquid(
 	S("Clay Watering Can with Salt Water"),
 	{dig_immediate = 2})
 
+-- add water to a node
+local function water_soil(itemstack, user, pointed_thing, water_source, node_suffix, empty_container)
+	-- if pointed thing is a soil block, water it
+	if pointed_thing.type == "node" then
+		local pos = pointed_thing.under
+		local node = minetest.get_node(pos)
+		if minetest.get_item_group(node.name, "sediment") > 0 then
+			-- check if watered block exists
+			local wet_node_name = node.name .. node_suffix
+			if minetest.registered_nodes[wet_node_name] then
+				-- replace with watered version
+				-- keeping the node orientation
+				minetest.set_node(pos, {name = wet_node_name, param2 = node.param2})
+				-- and empty the bucket
+				-- remove clay watering can and return empty one
+				itemstack:take_item()
+				return ItemStack("tech:clay_watering_can")
+			else
+				-- continue as normal
+				return liquid_store.on_use_filled_bucket(water_source, empty_container, itemstack, user, pointed_thing)
+			end
+		end
+	end
+	-- continue as normal
+	return liquid_store.on_use_filled_bucket(water_source, empty_container, itemstack, user, pointed_thing)
+end
 
 --make Watering can able to water a block on click
 minetest.override_item("tech:clay_watering_can_freshwater", {
 	on_use = function(itemstack, user, pointed_thing)
-		-- if pointed thing is a soil block, water it
-		if pointed_thing.type == "node" then
-			local pos = pointed_thing.under
-			local node = minetest.get_node(pos)
-			if minetest.get_item_group(node.name, "sediment") > 0 then
-				-- check if block exists
-				if minetest.registered_nodes[node.name.."_wet"] then
-					-- replace with watered version
-					-- keeping the node orientation
-					minetest.set_node(pos, {name = node.name.."_wet", param2 = node.param2})
-					-- and empty the bucket
-					-- remove clay watering can and return empty one
-					itemstack:take_item()
-					return ItemStack("tech:clay_watering_can")
-				else
-					-- continue as normal
-					return liquid_store.on_use_filled_bucket("nodes_nature:freshwater_source","tech:clay_watering_can",itemstack, user, pointed_thing)
-				end
-			end
-		end
-		-- continue as normal
-		return liquid_store.on_use_filled_bucket("nodes_nature:freshwater_source","tech:clay_watering_can",itemstack, user, pointed_thing)
-	end
+		return water_soil(itemstack, user, pointed_thing, "nodes_nature:freshwater_source", "_wet","tech:clay_watering_can")
+	end,
 })
 
+--make Watering can able to water a block on click
+minetest.override_item("tech:clay_watering_can_salt_water", {
+	on_use = function(itemstack, user, pointed_thing)
+		return water_soil(itemstack, user, pointed_thing, "nodes_nature:salt_water_source", "_wet_salty","tech:clay_watering_can")
+	end,
+})
--- mods/tech/crafts/watering_can.lua
+++ mods/tech/crafts/watering_can.lua
@@ -2,14 +2,14 @@
 crafting.register_recipe({
 	type = "clay_shaping_spot",
 	output = "tech:clay_watering_can_unfired 1",
-	items = {"nodes_nature:clay_wet 4"},
+	items = {"nodes_nature:clay_wet 5"},
 	level = 1,
 	always_known = true,
 })
 
 crafting.register_recipe({
 	type = "mixing_spot",
-	output = "nodes_nature:clay 4",
+	output = "nodes_nature:clay 5",
 	items = {"tech:clay_watering_can_unfired 1"},
 	level = 1,
 	always_known = true,
