e54c6724dde6caef8e76f99093c7948fab73aca4
Oscar Bernal <oscarbernal93@gmail.com>
2023-05-12 11:23:29 -0500

    change the crafting of the mortar bucket

--- mods/tech/crafts/mixing_bucket.lua
+++ mods/tech/crafts/mixing_bucket.lua
@@ -6,9 +6,9 @@ crafting.register_recipe({
 	always_known = true,
 })
 crafting.register_recipe({
-	type = "crafting_spot",
+	type = "mixing_bucket",
 	output = "tech:lime_mortar_bucket",
-	items = {"tech:stick 24", "tech:lime_mortar"},
+	items = {"tech:mixing_bucket", "tech:lime_mortar"},
 	level = 1,
 	always_known = true,
 })
