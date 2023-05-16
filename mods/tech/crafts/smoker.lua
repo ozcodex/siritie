ee7373f0a8c121260bdef8c9fab432351e2f663b
Oscar Bernal <oscarbernal93@gmail.com>
2023-05-15 23:51:59 -0500

    Adds the Smoker

---
+++ mods/tech/crafts/smoker.lua
@@ -0,0 +1,16 @@
+
+crafting.register_recipe({
+	type = "clay_shaping_spot",
+	output = "tech:smoker 1",
+	items = {"nodes_nature:clay_wet 5"},
+	level = 1,
+	always_known = true,
+})
+
+crafting.register_recipe({
+	type = "clay_mixing",
+	output = "nodes_nature:clay 5",
+	items = {"tech:smoker 1"},
+	level = 1,
+	always_known = true,
+})
